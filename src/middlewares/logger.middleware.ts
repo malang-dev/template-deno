3cf0951f946d831f679ae08f72299beff11417e2
Bramanda Suwandi <bramanda48@gmail.com>
2024-12-25 09:47:10 +0700

    fix: default logger message and request id

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,11 +1,13 @@
-import { RollbarHandler } from "@/utils/rollbar.ts";
+import { RollbarHandler } from "@/utils/logger.ts";
 import { ConsoleHandler, getLogger, setup as LoggerSetup } from "@std/log";
 import { generate } from "@std/uuid/unstable-v7";
 import { type Context, MiddlewareHandler } from "hono";
+import { getContext } from "hono/context-storage";
+import { getConnInfo } from "hono/deno";
 
 LoggerSetup({
   handlers: {
-    rollbar: new RollbarHandler("NOTSET", {
+    rollbar: new RollbarHandler("WARN", {
       enabled: Deno.env.get("ENABLE_ROLLBAR") === "true",
       environment: Deno.env.get("DENO_ENV"),
       accessToken: Deno.env.get("ROLLBAR_ACCESS_TOKEN"),
@@ -14,23 +16,24 @@ LoggerSetup({
     console: new ConsoleHandler("NOTSET", {
       useColors: true,
       formatter: (record) => {
-        const [reqId] = record.args as any[];
+        const ctx = getContext();
+        const ips = getConnInfo(ctx).remote.address;
+        const req = ctx.get("requestId");
+
         const date = record.datetime.toISOString();
         const name = record.loggerName;
 
-        let message = `${date} [${name}] [${reqId}] [${record.levelName}] -- ${record.msg}`;
-        if (record.loggerName !== "http") {
-          record.args.forEach((arg, index) => {
-            message += ` arg${index}: ` + JSON.stringify(arg);
-          });
-        }
-        return message;
+        const msgs = `${date} [${req}] [${name}] [${ips}] [${record.levelName}] -- ${record.msg}`;
+        const args = record.args.map((arg, index) => `arg${index}: ` + JSON.stringify(arg))
+          .join(" ");
+
+        return msgs + (name !== "http" ? " " + args : "");
       },
     }),
   },
   loggers: {
     default: {
-      level: "WARN",
+      level: "DEBUG",
       handlers: ["console", "rollbar"],
     },
     http: {
@@ -40,21 +43,12 @@ LoggerSetup({
   },
 });
 
-export const logger = (): MiddlewareHandler => {
-  const onRequestData = (ctx: Context): any => {
-    return {
-      url: ctx.req.path,
-      method: ctx.req.method,
-    };
-  };
-
-  const onResponseData = (ctx: Context): any => {
-    return {
-      status: ctx.res.status,
-      headers: ctx.res.headers,
-    };
-  };
-
+export function loggerMiddleware(): MiddlewareHandler {
+  const onRequestData = (ctx: Context): any => ({ url: ctx.req.path, method: ctx.req.method });
+  const onResponsData = (ctx: Context): any => ({
+    status: ctx.res.status,
+    headers: ctx.res.headers,
+  });
   return async function createMiddleware(ctx, next) {
     const reqId = generate();
     ctx.set("requestId", reqId);
@@ -62,11 +56,11 @@ export const logger = (): MiddlewareHandler => {
 
     const log = getLogger("http");
     const req = onRequestData(ctx);
-    log.info(`Request starting: ${req.method} ${req.url}`, reqId, req);
+    log.info(`Request starting: ${req.method} ${req.url}`);
 
     await next();
 
-    const res = onResponseData(ctx);
-    log.info(`Request complete: ${req.method} ${req.url} ${res.status}`, reqId, req, res);
+    const res = onResponsData(ctx);
+    log.info(`Request complete: ${req.method} ${req.url} ${res.status}`);
   };
-};
+}
